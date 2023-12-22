c9b87c915f7057137169c109a701f9bd27769ada
braizou <amirat.mehdi@gmail.com>
2023-12-22 09:46:21 +0100

    test

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+
+const CACHE_NAME = 'my-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/path/to/other/assets',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
